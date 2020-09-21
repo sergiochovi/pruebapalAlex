84d3473130bf6e03288a4d65f2a751c18ae87f90
sergiochovi <sgallardoq01@gmail.com>
2020-09-21 11:26:22 +0200

    Esto es una prueba netbeans al github

---
+++ pruebitaAlex/src/main/java/PruebaaMismo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sergi
+ */
+public class PruebaaMismo {
+    public static void main(String[] args) {
+        System.out.println("Esto es una prueba");
+    }
+}
